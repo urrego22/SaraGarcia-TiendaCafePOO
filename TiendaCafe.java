44aa27dab7354d57ad9f4112a06bddac5e2cd53d
Sara Garcia <urregosara984@gmail.com>
2025-11-27 15:16:36 -0500

    Añadir método aplicarDescuento a la clase Cafe

--- TiendaCafe.java
+++ TiendaCafe.java
@@ -6,7 +6,8 @@ public class TiendaCafe {
         Cafe cafe2 = new Cafe("Café del Huila", "Huila", 30000, 8);
         Cafe cafe3 = new Cafe("Café de Antioquia", "Antioquia", 28000, 12);
 
-        // Mostrar información
+        // Mostrar información inicial
+        System.out.println("--- Información inicial ---");
         cafe1.mostrarInformacion();
         cafe2.mostrarInformacion();
         cafe3.mostrarInformacion();
@@ -20,5 +21,21 @@ public class TiendaCafe {
         cafe1.actualizarCantidad(15);
         System.out.println("\n--- Información actualizada del Café de Nariño ---");
         cafe1.mostrarInformacion();
+
+        // Aplicar descuento del 10% a todos los cafés
+        cafe1.aplicarDescuento(10);
+        cafe2.aplicarDescuento(10);
+        cafe3.aplicarDescuento(10);
+
+        // Mostrar información después del descuento
+        System.out.println("\n--- Información después del descuento del 10% ---");
+        cafe1.mostrarInformacion();
+        cafe2.mostrarInformacion();
+        cafe3.mostrarInformacion();
+
+        // Mostrar precios totales actualizados
+        System.out.println("Nuevo precio total Café de Nariño: $" + cafe1.calcularPrecioTotal());
+        System.out.println("Nuevo precio total Café del Huila: $" + cafe2.calcularPrecioTotal());
+        System.out.println("Nuevo precio total Café de Antioquia: $" + cafe3.calcularPrecioTotal());
     }
 }
